f4295f83b0c9e91f7b0fb83785e7f83a01aa2ecb
zhangjiangwen <libai7236@gmail.com>
2023-04-27 11:37:51 +0800

    feature: Supplementary income calculation scenario

--- checklist/test_reward_allot.py
+++ checklist/test_reward_allot.py
@@ -51,18 +51,11 @@ class TestMint(object):
         assert end_balance_ug - superadmin_balance_ug == (end_block_height - start_block_height) * superadmin_amt_ug
 
     # TODO
-    #  1.活期各类场景叠加 计算收益
-    #         -.单独kyc收益
-    #         -.正常活期委托
-    #         -.永久活期委托 + 1kyc
-    #         -.周期活期委托 -> 质押有id 到期后提取 需确定只返回本金 + 利率算出来的金额 不包含活期收益   x + 10收益+1kyc
-    #                      -> 到期之后 根据利率算钱的那部分结束了，不提取的话 那质押的本金还享受活期收益
-
-    #         -.主动提取收益(tx withdraw) 和 被动提取
+    #  1.主动提取收益(tx withdraw) 和 被动提取
     #  2.出块减产逻辑
 
     def test_kyc_reward(self, setup_create_region):
-        """验证kyc的收益"""
+        """单独kyc收益"""
         region_admin_addr, region_id, region_name, _ = setup_create_region
 
         user_addr = self.test_kyc.test_new_kyc_user(dict(region_id=region_id, region_admin_addr=region_admin_addr))
@@ -81,7 +74,7 @@ class TestMint(object):
         assert delegate_info['interestAmount'] == x, f"{delegate_info['interestAmount']} != {x}"
 
     def test_delegate1_reward(self, setup_create_region):
-        """计算活期委托收益"""
+        """计算活期委托 + kyc 收益"""
         region_admin_addr, region_id, region_name, _ = setup_create_region
         user_addr = self.test_kyc.test_new_kyc_user(dict(region_id=region_id, region_admin_addr=region_admin_addr))
         send_data = dict(from_addr=f"{chain.super_addr}", to_addr=f"{user_addr}", amount="100", fees="1")
@@ -108,13 +101,14 @@ class TestMint(object):
 
         stage1_amt = ((stage2_start_height - 1) - kyc_start_height) * one_coin_reward_uc
         # 10 coin + 1 coin kyc
-        stage2_amt = (end_height - stage2_start_height) * (one_coin_reward_uc * 11)
-        reward = (stage1_amt * 1000000 + stage2_amt * 1000000) / 1000000
+        stage2_amt = (end_height - stage2_start_height) * \
+                     (one_coin_reward_uc * 11 * chain.float_precision) / chain.float_precision
+        reward = (stage1_amt * chain.float_precision + stage2_amt * chain.float_precision) / chain.float_precision
         logger.info(f"stage1_amt: {stage1_amt}, stage2_amt: {stage2_amt}, reward: {reward}")
         assert str(float(interest)) == str(reward)
 
     def test_delegate2_fixed(self, setup_create_region):
-        """计算活期周期委托收益"""
+        """计算活期周期委托 + kyc 收益"""
         region_admin_addr, region_id, region_name, _ = setup_create_region
         user_addr = self.test_kyc.test_new_kyc_user(dict(region_id=region_id, region_admin_addr=region_admin_addr))
         send_data = dict(from_addr=f"{chain.super_addr}", to_addr=f"{user_addr}", amount="100", fees="1")
@@ -141,13 +135,14 @@ class TestMint(object):
 
         stage1_amt = ((stage2_start_height - 1) - kyc_start_height) * one_coin_reward_uc
         # 10 coin + 1 coin kyc
-        stage2_amt = (end_height - stage2_start_height) * (one_coin_reward_uc * 11)
-        reward = (stage1_amt * 1000000 + stage2_amt * 1000000) / 1000000
+        stage2_amt = (end_height - stage2_start_height) * (
+                one_coin_reward_uc * 11 * chain.float_precision) / chain.float_precision
+        reward = (stage1_amt * chain.float_precision + stage2_amt * chain.float_precision) / chain.float_precision
         logger.info(f"stage1_amt: {stage1_amt}, stage2_amt: {stage2_amt}, reward: {reward}")
         assert str(float(interest)) == str(reward)
 
-    def test_delegate3_fixed(self, setup_create_region):
-        """计算活期永久委托收益"""
+    def test_delegate3_infinite(self, setup_create_region):
+        """计算活期永久委托 + kyc 收益"""
         region_admin_addr, region_id, region_name, _ = setup_create_region
         user_addr = self.test_kyc.test_new_kyc_user(dict(region_id=region_id, region_admin_addr=region_admin_addr))
         send_data = dict(from_addr=f"{chain.super_addr}", to_addr=f"{user_addr}", amount="100", fees="1")
@@ -174,14 +169,14 @@ class TestMint(object):
 
         stage1_amt = ((stage2_start_height - 1) - kyc_start_height) * one_coin_reward_uc
         # 10 coin + 1 coin kyc
-        stage2_amt = (end_height - stage2_start_height) * (one_coin_reward_uc * 11)
-        reward = (stage1_amt * 1000000 + stage2_amt * 1000000) / 1000000
+        stage2_amt = (end_height - stage2_start_height) * \
+                     (one_coin_reward_uc * 11 * chain.float_precision) / chain.float_precision
+        reward = (stage1_amt * chain.float_precision + stage2_amt * chain.float_precision) / chain.float_precision
         logger.info(f"stage1_amt: {stage1_amt}, stage2_amt: {stage2_amt}, reward: {reward}")
         assert str(float(interest)) == str(reward)
 
-    def test_delegate_group(self, setup_create_region):
-        """委托组合场景"""
-        # TODO 待修复
+    def test_delegate123_group(self, setup_create_region):
+        """委托组合场景收益计算"""
         region_admin_addr, region_id, region_name, _ = setup_create_region
         user_addr = self.test_kyc.test_new_kyc_user(dict(region_id=region_id, region_admin_addr=region_admin_addr))
         send_data = dict(from_addr=f"{chain.super_addr}", to_addr=f"{user_addr}", amount="100", fees="1")
@@ -200,7 +195,8 @@ class TestMint(object):
         time.sleep(10)
         delegate_info = self.handle_q.get_delegate(user_addr)['delegation']
         delegate_start_height = int(delegate_info['startHeight'])
-        logger.info(f"stage2_start_height: {delegate_start_height}")
+        logger.info(f"delegate_start_height: {delegate_start_height}")
+        # 第一部分 只有kyc收益
         stage1_amt = ((delegate_start_height - 1) - kyc_start_height) * one_coin_reward_uc
 
         del_data = dict(region_user_addr=f"{user_addr}", amount=10, term=chain.delegate_term[1], fees=1)
@@ -209,8 +205,9 @@ class TestMint(object):
         delegate_fixed_info = self.handle_q.get_delegate(user_addr)['delegation']
         delegate_fixed_start_height = int(delegate_fixed_info['startHeight'])
 
-        # 10 coin + 1 coin kyc
-        stage2_amt = (delegate_fixed_start_height - delegate_start_height) * (one_coin_reward_uc * 11)
+        # 第二部分 活期委托+kyc收益
+        stage2_amt = ((delegate_fixed_start_height - 1) - delegate_start_height) * \
+                     (one_coin_reward_uc * 11 * chain.float_precision) / chain.float_precision
 
         del_data = dict(region_user_addr=f"{user_addr}", amount="10", fees="1")
         self.test_del.test_delegate_infinite(del_data)
@@ -218,17 +215,106 @@ class TestMint(object):
         delegate_infinite_info = self.handle_q.get_delegate(user_addr)['delegation']
         delegate_infinite_start_height = int(delegate_infinite_info['startHeight'])
 
-        # 20 coin + 1 coin kyc
-        stage3_amt = (delegate_infinite_start_height - delegate_fixed_start_height) * (one_coin_reward_uc * 21)
+        # 第三部分 活期委托+kyc收益+活期内周期收益
+        stage3_amt = ((delegate_infinite_start_height - 1) - delegate_fixed_start_height) * \
+                     (one_coin_reward_uc * 21 * chain.float_precision) / chain.float_precision
 
-        new_info = self.handle_q.get_delegate(user_addr)['delegation']
-        interest = new_info['interestAmount']
+        interest = self.handle_q.get_delegate(user_addr)['delegation']['interestAmount']
         end_height = self.handle_q.get_block()
-        print(f"块度相差 {int(new_info['startHeight'])} == {end_height}")
 
-        stage4_amt = (end_height - delegate_infinite_start_height) * (one_coin_reward_uc * 31)
+        # 第四部分 活期委托+kyc收益+活期内周期收益+永久活期收益
+        stage4_amt = (end_height - delegate_infinite_start_height) * \
+                     (one_coin_reward_uc * 31 * chain.float_precision) / chain.float_precision
 
-        reward = (stage1_amt * 1000000 + stage2_amt * 1000000 + stage3_amt * 1000000 + stage4_amt * 1000000) / 1000000
+        reward = (stage1_amt * chain.float_precision + stage2_amt * chain.float_precision +
+                  stage3_amt * chain.float_precision + stage4_amt * chain.float_precision) / chain.float_precision
         logger.info(f"stage1_amt: {stage1_amt}, stage2_amt: {stage2_amt}, "
                     f"stage3_amt: {stage3_amt}, stage4_amt: {stage4_amt}, reward: {reward}")
         assert str(float(interest)) == str(reward)
+
+    def test_delegate2_fixed_expire(self, setup_create_region):
+        """活期周期委托到期后收益计算
+            -> 到期之后 不提取的话 那质押本金还享受活期收益"""
+        region_admin_addr, region_id, region_name, _ = setup_create_region
+        user_addr = self.test_kyc.test_new_kyc_user(dict(region_id=region_id, region_admin_addr=region_admin_addr))
+        send_data = dict(from_addr=f"{chain.super_addr}", to_addr=f"{user_addr}", amount="100", fees="1")
+        self.test_bank.test_send(send_data)
+
+        # kyc 每个块收益
+        region_info = self.handle_q.get_region(region_id)['region']
+        one_coin_reward_uc = self.region_amt_uc * (calculate.to_usrc(1) / int(region_info['regionTotalUAC']))
+        logger.info(f"one_coin_reward_uc: {one_coin_reward_uc}")
+
+        kyc_start_height = int(self.handle_q.get_delegate(user_addr)['delegation']['startHeight'])
+        logger.info(f"kyc_start_height: {kyc_start_height}")
+
+        # 发起活期质押后 会更新startHeight
+        del_data = dict(region_user_addr=f"{user_addr}", amount=10, term=chain.delegate_term[3], fees=1)
+        self.test_del.test_delegate_fixed(del_data)
+
+        time.sleep(90 + 10)  # 3 * 30 = 90天 90s < 100s 已到期
+
+        delegate_info = self.handle_q.get_delegate(user_addr)['delegation']
+        interest = delegate_info['interestAmount']
+        end_height = self.handle_q.get_block()
+        stage2_start_height = int(delegate_info['startHeight'])
+        logger.info(f"stage2_start_height: {stage2_start_height}, end_height: {end_height}, interestAmount: {interest}")
+
+        stage1_amt = ((stage2_start_height - 1) - kyc_start_height) * one_coin_reward_uc
+        # 10 coin + 1 coin kyc  按最新块计算,即到期后不提取还享受活期收益
+        stage2_amt = (end_height - stage2_start_height) * (
+                (one_coin_reward_uc * 11) * chain.float_precision) / chain.float_precision
+        reward = (stage1_amt * chain.float_precision + stage2_amt * chain.float_precision) / chain.float_precision
+        logger.info(f"stage1_amt: {stage1_amt}, stage2_amt: {stage2_amt}, reward: {reward}")
+        assert str(float(interest)) == str(reward)
+
+    def test_delegate2_fixed_withdraw(self, setup_create_region):
+        """活期内周期提取 -> 本金 + 利息收益 + (三种类型所有)活期收益"""
+        region_admin_addr, region_id, region_name, _ = setup_create_region
+        user_addr = self.test_kyc.test_new_kyc_user(dict(region_id=region_id, region_admin_addr=region_admin_addr))
+        send_data = dict(from_addr=f"{chain.super_addr}", to_addr=f"{user_addr}", amount="100", fees="1")
+        self.test_bank.test_send(send_data)
+
+        # kyc 每个块收益
+        region_info = self.handle_q.get_region(region_id)['region']
+        one_coin_reward_uc = self.region_amt_uc * (calculate.to_usrc(1) / int(region_info['regionTotalUAC']))
+        logger.info(f"one_coin_reward_uc: {one_coin_reward_uc}")
+        kyc_start_height = int(self.handle_q.get_delegate(user_addr)['delegation']['startHeight'])
+        logger.info(f"kyc_start_height: {kyc_start_height}")
+
+        # 发起活期质押后 会更新startHeight
+        del_data = dict(region_user_addr=f"{user_addr}", amount=10, term=chain.delegate_term[3], fees=1)
+        self.test_del.test_delegate_fixed(del_data)
+
+        user_addr_balance_uc = int(self.handle_q.get_balance(user_addr, chain.coin['uc'])["amount"])
+        logger.info(f"user_addr_balance_uc: {user_addr_balance_uc}")
+
+        time.sleep(90 + 10)
+
+        delegate_info = self.handle_q.get_delegate(user_addr)['delegation']
+        interest = delegate_info['interestAmount']
+        stage2_start_height = int(delegate_info['startHeight'])
+
+        fixed_delegate_info = self.handle_q.q.staking.show_fixed_delegation(user_addr)
+        fixed_delegation_id = fixed_delegate_info['items'][0]['id']
+        undelegate_fixed_data = dict(from_addr=user_addr, fixed_delegation_id=fixed_delegation_id, fees=1)
+        self.test_del.test_undelegate_fixed(undelegate_fixed_data)
+        end_height = self.handle_q.get_block()
+        logger.info(f"stage2_start_height: {stage2_start_height}, end_height: {end_height}, interestAmount: {interest}")
+
+        stage1_amt = ((stage2_start_height - 1) - kyc_start_height) * one_coin_reward_uc
+        stage2_amt = (end_height - stage2_start_height) * (
+                (one_coin_reward_uc * 11) * chain.float_precision) / chain.float_precision
+
+        reward = (stage1_amt * chain.float_precision + stage2_amt * chain.float_precision) / chain.float_precision
+        logger.info(f"stage1_amt: {stage1_amt}, stage2_amt: {stage2_amt}, reward: {reward}")
+        accrual = chain.annualRate[3] * 3 / 12 * (10 * 10 ** 6)
+        logger.info(f"accrual: {accrual}uc")
+
+        user_addr_balance_uc = int(self.handle_q.get_balance(user_addr, chain.coin['uc'])["amount"])
+        user_addr_balance_ug = int(self.handle_q.get_balance(user_addr, chain.coin['ug'])["amount"])
+
+        logger.info(f"user_addr_balance_uc: {user_addr_balance_uc}, user_addr_balance_ug: {user_addr_balance_ug}")
+
+        assert user_addr_balance_uc == calculate.to_usrc(100 - 1 - 1) + int(accrual) + int(reward)
+        assert user_addr_balance_ug == int(accrual) + int(reward)
--- main.py
+++ main.py
@@ -8,13 +8,8 @@ from loguru import logger
 logger.add("logs/case_{time}.log", rotation="500MB")
 
 if __name__ == '__main__':
-    '''
-    -q: 安静模式, 不输出环境信息
-    -v: 丰富信息模式, 输出更详细的用例执行信息
-    -s: 显示程序中的print/logging输出
-    '''
     start_time = time.time()
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    pytest.main(['-s', "checklist/", 'lf', f'--alluredir={BASE_DIR}/allure-results'])
+    pytest.main(['-s', "checklist/", f'--alluredir={BASE_DIR}/allure-results'])
     elapsed_time = (time.time() - start_time) / 60 / 60  # 计算时间差
     logger.info(f"Elapsed time: {elapsed_time} hours")
