35e6a5edd33ca962bbefbe35d93b493de530d4cd
tangyihang <tyhdark@gmail.com>
2023-08-15 13:50:42 +0800

    新建三个模块测试

--- cases/stake/delegate.py
+++ cases/stake/delegate.py
@@ -20,7 +20,7 @@ from x.tx import Tx
 #     q = Query()
 
 
-@pytest.mark.skip
+
 class TestDelegate(object):
     test_region = unitcases.Region()
     test_del = unitcases.Delegate()
@@ -31,11 +31,21 @@ class TestDelegate(object):
     base_cfg = test_bank.tx
     user_addr = None
 
-    def test_newkey_unkyc_delegate(self):
+    def test_new_key_no_kyc_delegate(self):
         """新创建的用户，没有kyc，没有余额时进行活期委托"""
-        logger.info("TestDelegate/test_newkey_unkyc_delegate")
-        user_info = self.test_key.test_add()
-        user_addr = user_info['address']
-        # 查询当前用户是不是没有余额
-        user_balance = HttpResponse.get_balance_unit(user_addr)
-        assert user_balance == 0
+        logger.info("TestDelegate/test_new_key_no_kyc_delegate")
+        try:
+            user_info = self.test_key.test_add()
+            user_addr = user_info['address']
+            # 查询当前用户是不是没有余额
+            user_balance = HttpResponse.get_balance_unit(user_addr)
+            assert user_balance == 0
+            #没有余额的情况下进行活期委托
+
+        except Exception:
+            logger.info("ERROR", Exception.args)
+
+        finally:
+            logger.info("delete-------------------->"+user_info['name'])
+            self.test_key.test_delete_key(user_addr)
+
